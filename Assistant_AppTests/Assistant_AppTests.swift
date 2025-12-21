47173abf25c612c706a10dc7350b8f0d07d6f48b
TerenceWin <105475764+TerenceWin@users.noreply.github.com>
2025-12-21 09:11:10 +0900

    Initial Commit

---
+++ Assistant_AppTests/Assistant_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Assistant_AppTests.swift
+//  Assistant_AppTests
+//
+//  Created by Terence Win on 2025/12/21.
+//
+
+import Testing
+@testable import Assistant_App
+
+struct Assistant_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
